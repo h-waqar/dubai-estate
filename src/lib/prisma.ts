6fd20bef6cbe3dac4bf60f5c210996d7c4e229ef
h-waqar <hamzawaqar556@gmail.com>
2025-10-18 15:26:12 +0500

    init: Windows.exe

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,13 @@
 // lib/prisma.ts
 import { PrismaClient } from "@/generated/prisma";
+// import { PrismaClient } from "../generated/prisma";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ["query", "info", "warn", "error"], // helps during dev
-    });
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // helps during dev
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
