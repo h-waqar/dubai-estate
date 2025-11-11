fa4f893d088098bfa1a1c8c1e8c89070f4079dff
h-waqar <hamzawaqar556@gmail.com>
2025-11-11 12:23:20 +0500

    Design: Step 1

--- src/app/(frontend)/advertise/page.tsx
+++ src/app/(frontend)/advertise/page.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/layout/Header";
 import AdvertiseWizard from "@/modules/property/components/advertise/AdvertiseWizard";
 import StepHeader from "@/modules/property/components/advertise/StepHeader";
 
 function AdvertisePage() {
   return (
     <>
+      <Header />
       <AdvertiseWizard />
     </>
   );
--- src/modules/property/components/advertise/steps/StepOneCreate.tsx
+++ src/modules/property/components/advertise/steps/StepOneCreate.tsx
@@ -3,6 +3,8 @@
 import { Input } from "@/components/ui/input";
 import StepController from "./StepController";
 import { useAdvertiseStore } from "../../../stores/useAdvertiseStore";
+import LocationSelector from "../LocationSelector";
+import Header from "@/components/layout/Header";
 
 function StepOneCreate() {
   const { data, updateData, next, prev } = useAdvertiseStore();
@@ -10,12 +12,13 @@ function StepOneCreate() {
   return (
     <div className="space-y-6">
       <div>
-        <label className="block text-sm mb-1">Property Title</label>
+        {/* <label className="block text-sm mb-1">Property Title</label>
         <Input
           value={data.title || ""}
           onChange={(e) => updateData({ title: e.target.value })}
           placeholder="Luxury Apartment in Downtown"
-        />
+        /> */}
+        <LocationSelector />
       </div>
 
       <StepController onNext={next} onPrev={prev} showPrev={false} />
