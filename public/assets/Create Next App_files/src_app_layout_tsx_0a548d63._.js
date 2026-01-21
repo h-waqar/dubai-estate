3a26ea72f877f0a4b7a491b8de3b635aab668c4e
Hamza Waqar <hamzawaqar556@gmail.com>
2026-01-21 13:11:47 +0500

    Chore: Regenrate Migrations

--- public/assets/Create Next App_files/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/node_modules_344406a3._.js",
-  "static/chunks/src_components_ff3cf546._.js"
-],
-    source: "dynamic"
-});
