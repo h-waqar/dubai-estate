e9195699536e1c443f35f04bb2006cd5aacc934e
h-waqar <hamzawaqar556@gmail.com>
2026-03-12 23:20:30 +0500

    Changes to docker.prod

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,20 +1,3 @@
-// // prisma.config.ts
-// import "dotenv/config";
-// import { defineConfig, env } from "prisma/config";
-
-// export default defineConfig({
-//   schema: "prisma/schema.prisma",
-//   migrations: {
-//     path: "prisma/migrations",
-//     seed: "tsx prisma/seed.ts",
-//   },
-//   datasource: {
-//     url: env("DIRECT_URL") ?? env("DATABASE_URL"),
-//     // shadowDatabaseUrl: env("SHADOW_DATABASE_URL"), // optional if you use one
-//   },
-// });
-
-
 // prisma.config.ts
 import { defineConfig } from "prisma/config";
 
