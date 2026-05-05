f34c10e93ed606962a619a41ad05f36fe6777986
hwaqar <hamzawaqar556@gmail.com>
2026-05-05 16:01:49 +0500

    feat(quick-reset-db-01): delete existing migrations

--- prisma/migrations/20260226090657_add_project_proposed_developer/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Project" DROP CONSTRAINT "Project_developerId_fkey";
-
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "proposedDeveloperName" TEXT,
-ALTER COLUMN "developerId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Project" ADD CONSTRAINT "Project_developerId_fkey" FOREIGN KEY ("developerId") REFERENCES "Developer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20260430160000_add_applicable_to_entitlements/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "EntityType" AS ENUM ('PROPERTY', 'PROJECT', 'ALL');
-
--- AlterTable
-ALTER TABLE "EntitlementDefinition" ADD COLUMN "applicableTo" "EntityType" NOT NULL DEFAULT 'PROPERTY';
